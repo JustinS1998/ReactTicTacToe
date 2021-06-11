ac89f78e98661dc66ae5a9a39cc3397174ae7bc5
Justin Seitz <justinseitz2016@gmail.com>
2021-06-11 16:01:51 -0700

    buttons can now toggle turns

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ class Square extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
-        this.setState({mark: 'x'});
+        this.setState(this.props.turn === 'x' ? {mark: 'x'} : {mark: 'o'});
+        this.props.buttonClick();
     }
     render() {
         return (
@@ -29,7 +30,7 @@ class Board extends React.Component {
         return (
             <div className='board'>
                 <div className='row'>
-                    <Square num='0' />
+                    <Square num='0' turn={this.props.turn} buttonClick={this.props.buttonClick} />
                     <Square num='1' />
                     <Square num='2' />
                 </div>
@@ -49,11 +50,22 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            turn: 'x'
+        };
+        this.toggleTurn = this.toggleTurn.bind(this);
+    }
+    toggleTurn() {
+        console.log('Toggling');
+        this.setState(this.state.turn === 'x' ? {turn: 'o'} : {turn: 'x'});
+    }
     render() {
         return (
             <>
-                <h1>Tic Tac Toe</h1>
-                <Board />
+                <h1>Turn: {this.state.turn}</h1>
+                <Board turn={this.state.turn} buttonClick={this.toggleTurn} />
             </>
         );
     }
