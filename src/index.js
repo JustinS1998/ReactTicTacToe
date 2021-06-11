a883b238b0a6b4db0c8a52c6ec857465c375a938
Justin Seitz <justinseitz2016@gmail.com>
2021-06-11 15:39:53 -0700

    can now click on boxes to add an x

--- src/index.js
+++ src/index.js
@@ -3,30 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-        return <button className='square'></button>
+    constructor(props) {
+        super(props);
+        this.state = {
+            mark: ' '
+        }
+        this.handleClick = this.handleClick.bind(this);
     }
-
+    handleClick() {
+        this.setState({mark: 'x'});
+    }
+    render() {
+        return (
+        <button 
+            className='square' 
+            num={this.props.num}
+            onClick={this.handleClick}
+        >{this.state.mark}</button>
+        )
+    }
 }
 
 class Board extends React.Component {
     render() {
         return (
-            <div class='board'>
-                <div class='row'>
-                    <Square />
-                    <Square />
-                    <Square />
+            <div className='board'>
+                <div className='row'>
+                    <Square num='0' />
+                    <Square num='1' />
+                    <Square num='2' />
                 </div>
-                <div class='row'>
-                    <Square />
-                    <Square />
-                    <Square />
+                <div className='row'>
+                    <Square num='3' />
+                    <Square num='4' />
+                    <Square num='5' />
                 </div>
-                <div class='row'>
-                    <Square />
-                    <Square />
-                    <Square />
+                <div className='row'>
+                    <Square num='6' />
+                    <Square num='7' />
+                    <Square num='8' />
                 </div>
             </div>
         );
