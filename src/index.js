852a3fd7f5cd2e3720670cf8cc491f6ea5c9f091
Justin Seitz <justinseitz2016@gmail.com>
2021-06-11 14:53:23 -0700

    added button and css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-function MyComponent () {
-    return <h1>Hello world</h1>;
+function Square () {
+    return <button 
+    className='square'>a</button>
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById('root'));
+function Board () {
+    const renderSquare = () => {
+        return <Square />;
+    }
+    return (
+        <div class='board'>
+            {renderSquare()}
+        </div>
+    );
+}
+
+function Game () {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <Board />
+        </>
+    );
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'));
