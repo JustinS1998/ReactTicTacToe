daaaf9cf95d1b57a1e0b7949f18e557b5bb51dda
Justin Seitz <justinseitz2016@gmail.com>
2021-06-11 17:05:33 -0700

    now displays winner

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class Square extends React.Component {
             this.setState({ mark: this.props.turn });
             this.props.toggleTurn();
             this.props.updateSquares(this.props.num);
+            this.props.calculateWinner();
         }
     }
     render() {
@@ -33,7 +34,8 @@ class Board extends React.Component {
             num={i}
             turn={this.props.turn}
             toggleTurn={this.props.toggleTurn}
-            updateSquares={this.props.updateSquares} />
+            updateSquares={this.props.updateSquares}
+            calculateWinner={this.props.calculateWinner} />
     }
     render() {
         return (
@@ -63,10 +65,12 @@ class Game extends React.Component {
         super(props);
         this.state = {
             turn: 'x',
-            squares: Array(9).fill('')
+            squares: Array(9).fill(''),
+            winner: ''
         };
         this.toggleTurn = this.toggleTurn.bind(this);
         this.updateSquares = this.updateSquares.bind(this);
+        this.calculateWinner = this.calculateWinner.bind(this);
     }
     toggleTurn() {
         this.setState(this.state.turn === 'x' ? { turn: 'o' } : { turn: 'x' });
@@ -92,19 +96,24 @@ class Game extends React.Component {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
+                //return squares[a];
+                this.setState({ winner: squares[a] });
+                return;
             }
         }
-        return '';
+        //return '';
+        this.setState({ winner: '' });
     }
     render() {
         return (
             <>
                 <h1>Turn: {this.state.turn}</h1>
+                <h2>Winner: {this.state.winner}</h2>
                 <Board 
                     turn={this.state.turn} 
                     toggleTurn={this.toggleTurn}
-                    updateSquares={this.updateSquares} />
+                    updateSquares={this.updateSquares}
+                    calculateWinner={this.calculateWinner} />
             </>
         );
     }
